13ab1501a7732b277c183a9b0d62e657f99b2ee2
root <root@KITT.(none)>
2015-05-12 10:09:52 -0700

    added undistort points script

--- broken_moth_tracker.cpp
+++ broken_moth_tracker.cpp
@@ -29,8 +29,6 @@
 #include <boost/lexical_cast.hpp> //  string concatination with doubles
 #include <math.h>
 #include <limits.h> // access quiet NAN
-#include <algorithm> // for max heap
-#include <vector>
 
 
 #define _USE_MATH_DEFINES
@@ -39,15 +37,9 @@ using namespace cv;
 using namespace std;
 using namespace boost;
 
-
-typedef numeric_limits<float> LIMITS;
-
 int const DEPTH = CV_16S;// 16short is used to prevent overflow during gradient cal
 int const SCALE = 1,DELTA = 0,RADIUS = 5;
-int const MIN_AREA = 50;  // *** increasing this value --> more spotty trajectory *** //
-int const MAX_AREA = 500;
-int const ROI_HEIGHT = 50;
-
+int const MIN_AREA = 200;  // *** increasing this value --> more spotty trajectory *** //
 /* Calibration parameters used to undistort points */
 //Matx33f const camera_mat( 233.7251, 0,         323.25797,\
 //                          0,        233.01072, 238.90632,\
@@ -82,9 +74,7 @@ void getBGModel(char* video_file);
 void draw(Point);
 void drawForeground();
 Point retrieveAvg(vector<int>,int);
-Point2f findObjectCentroid();
-bool isNan(float);
-void reportCentroid();
+void getCentroid();
 void rectifySrc(Mat*,Mat*);
 void segObjects();
 void writeToVideo(VideoWriter* outputVideo,bool output_result);
@@ -95,9 +85,6 @@ void processVideo(char* video_file);
 int main(int argc, char* argv[])
 {
   pMOG2 = new BackgroundSubtractorMOG2(10,16,false); //MOG2 approach
-
-
-  findObjectCentroid();
   //check for the input parameter correctness
   if(argc != 3)
   {
@@ -109,7 +96,6 @@ int main(int argc, char* argv[])
   data_out.open(argv[2]);
   //initialize background
   getBGModel(argv[1]);
-//  bgSet=true;
   //input data coming from a video
   if(bgSet){ processVideo(argv[1]); }
 
@@ -144,131 +130,52 @@ void drawForeground()
 // returns average position between all segments that exceed the threshold
 Point retrieveAvg(vector<int> a,int asize)
 { // a contains INDECES of contours
+  vector< Moments > mu(asize); // central moments
+  vector< Point2f > centroids(asize); // centers of mass
   Point avg; //avg.x=0; avg.y=0;
-  Moments mu;
 
-// *** cleaned up ***
   // calculate centroids of each contour
   for(int i=0; i<asize; i++)
   {
     // get central moments
-    mu = moments( contours[a[i]], false ); // true --> binary image
+    mu[i] = moments( contours[a[i]], false ); // true --> binary image
     // calc mass center based on spacial moments x=m10/m00, y=m01/m00;
-    Point ci = Point2f( mu.m10/mu.m00, mu.m01/mu.m00 );
-    // skip averaging if less than two candidates
-    if(asize == 1){ return ci; }
-//    return ci;
-    avg.x += ci.x; avg.y += ci.y;
+    centroids[i] = Point2f( mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00 );
   }
+
+  // skip averaging if less than two candidates
+  if(asize == 1){ return centroids[0]; }
+
+  // otherwise, calc the average centroid
+  for(int i=0; i < asize; i++)
+  {
+      Point ci = centroids[i];
+      avg.x += ci.x; avg.y += ci.y;
+  }
+
+// *** cleaned up ***
+//  // calculate centroids of each contour
+//  for(int i=0; i<asize; i++)
+//  {
+//    // get central moments
+//    mu = moments( contours[a[i]], false ); // true --> binary image
+//    // calc mass center based on spacial moments x=m10/m00, y=m01/m00;
+//    Point ci = Point2f( mu.m10/mu.m00, mu.m01/mu.m00 );
+//    // skip averaging if less than two candidates
+//    if(asize == 1){ return ci; }
+
+//    avg.x += ci.x; avg.y += ci.y;
+//  }
+
+
+
+
   avg.x /= asize; avg.y /= asize;
   return avg;
 }
 
-//// returns position of largest contour and checks calculated pos against prev
-//Point findObjectCentroid()
-//{
-//  Point centroid;
-//  // vector: threshed areas
-//  // heap: keep areas
-//  // rectange: roi
-//  // bool: search_flag=false
-//  // int: largestArea,height
-
-
-//  // height=INIT_HEIGHT
-
-//  // if area larger than thresh:
-//  //   copy to vector and count
-
-//  // make_heap using vector
-
-//  // largestArea=pop_heap
-//  // calc centroid for contour at index of largestArea
-
-//  // if history is null:
-//  //   return centroid
-//  // else:
-//  //   search_flag=true
-//  //   create roi centered at history (call createROI)
-
-//  // while search:
-//  //   for ii in size vector:
-//  //      if centroid in roi: return centroid
-//  //      largestArea=pop_heap
-//  //      calc centroid for contour at index of largestArea
-//  //
-//  //   // didn't find contour in roi
-//  //   expandROI by 2*height
-//  //   make_heap using vector
-
-
-//  // update
-//  // history=centroid
-
-//  return centroid;
-//}
-
-Point2f history=Point2f(LIMITS::quiet_NaN(),LIMITS::quiet_NaN());
-
-Point2f findObjectCentroid()
-{
-  vector< double > thresh_areas(contours.size());
-  int area_to_contour[contours.size()];
-  double largest_area[5];
-  Point2f cc; Rect roi; Moments mu;
-  int area_count=0,height=ROI_HEIGHT;
-  bool found=false;
-
-  // calaculate and threshold areas
-  for(int ii=0; ii<contours.size(); ii++)
-  {
-    double area=contourArea(contours[ii]);
-    if(MIN_AREA<area && area<MAX_AREA)
-    {
-      thresh_areas.push_back(area); area_count++;
-      area_to_contour[ii]=area; // keep track of which contour the area belongs to
-    }
-    area_to_contour[ii]=-1; // mark areas outside threshold bounds
-  }
-  if(area_count==0){ return Point2f(LIMITS::quiet_NaN(),LIMITS::quiet_NaN()); } // no detectable obj
-//    mu = moments( contours[], false ); // true --> binary image
-//    // calc mass center based on spacial moments x=m10/m00, y=m01/m00;
-//    Point ci = Point2f( mu.m10/mu.m00, mu.m01/mu.m00 );
-
-
-
-  // create max heap of all areas and collect top ten
-  make_heap(thresh_areas.begin(),thresh_areas.end());
-  for(int ii=0; ii<5; ii++)
-  {
-    if(ii<=area_count){
-      largest_area[ii]=thresh_areas.front();
-      pop_heap(thresh_areas.begin(),thresh_areas.end());
-      cout<<"area "<<ii<<"="<<largest_area[ii]<<endl;
-    }
-    else{
-      largest_area[ii]=0.f;
-      cout<<"area "<<ii<<"="<<largest_area[ii]<<endl;
-    }
-  }
-
-//  while(!found)
-//  {
-
-
-
-//  }
-
-  return cc;
-}
-
-// checks if val is nan
-bool isNan(float d){  return d != d; }
-// createROI(int height,Point center)
-// expandROI(int newHeight)
-
 // calculates and displays centroid of objects with size > threshold value
-void reportCentroid()
+void getCentroid()
 {
   vector< double > areas(contours.size());
   vector< int > icandidates(contours.size()); // INDEX of candidate contours
@@ -281,12 +188,10 @@ void reportCentroid()
     // calculate area
     areas[i] = contourArea(contours[i]);
     // for multiple centroids passing threshold, take only the largest centroid (i.e. ignore reflections)
-    if( areas[i] > MIN_AREA && areas[i] < MAX_AREA){ icandidates[n_cands++] = i; }
+    if( areas[i] > MIN_AREA ){ icandidates[n_cands++] = i; }
   }
   if( n_cands > 0 )
   {
-
-    //draw these n contours 			//INFO//
     centroid = retrieveAvg(icandidates,n_cands);
     // Write position to file
     data_out << lexical_cast<string>(frameID) +","+ \
@@ -298,28 +203,16 @@ void reportCentroid()
   { // Display only last known centriod
     // Write nan to file b/c no detectable object
     //ROS_INFO("Object not found: ignore or reduce threshold");
-
 #ifdef NAN
     data_out << lexical_cast<string>(frameID) +","+ \
-                lexical_cast<string>(LIMITS::quiet_NaN()) +","+ \
-                lexical_cast<string>(LIMITS::quiet_NaN()) << \
+                lexical_cast<string>(numeric_limits<double>::quiet_NaN()) +","+ \
+                lexical_cast<string>(numeric_limits<double>::quiet_NaN()) << \
                 '\n';
 #endif
   }
-
-// non-averaging (get max)
-//  centroid=findObjectCentroid();
-//  // Write position to file
-//  data_out << lexical_cast<string>(frameID) +","+ \
-//              lexical_cast<string>(centroid.x) +","+ \
-//              lexical_cast<string>(centroid.y) << \
-//              '\n';
-
-
   draw(centroid);
   //printf( "[%d]\t\t(%d,%d)\n", frameID, centroid.x, centroid.y );  //INFO//
 }
-
 
 // Averages all frames within video file into one image which represents the initial background model
 // NOTE: the frames are read in as unsigned char type, then converted to floating point type to compute
@@ -374,7 +267,6 @@ void getBGModel(char* videoFilename)
   cout << "Initial background is set.\n"; //INFO//
 }
 
-
 void rectifySrc(Mat* src,Mat* rect)
 {
   try
@@ -386,16 +278,14 @@ void rectifySrc(Mat* src,Mat* rect)
   }
 }
 
-
 // finds objects in the foreground of a frame, then displays their location
 void segObjects()
 {
   Mat mask = fgMaskMOG2.clone(); // avoid altering fgmask --> make deep copy
-  findContours( mask, contours, heirarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE, Point(0,0) );
+  findContours( mask, contours, heirarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0) );
   drawForeground();
-  reportCentroid();
+  getCentroid();
 }
-
 
 void writeToVideo(VideoWriter* outputVideo,bool output_tracking_result)
 {
@@ -428,6 +318,7 @@ void framePercentProgress(VideoCapture* cap)
   //cout << percentProgress << endl;
 
 }
+
 
 // captures frames from a video file, then detects obejects in the foreground and displays them
 void processVideo(char* videoFilename)
@@ -472,9 +363,9 @@ void processVideo(char* videoFilename)
     pMOG2->operator()(frame_rectified,fgMaskMOG2);
     // segment objects larger than maximum threshold (ignore noise)
     segObjects();
-//    // add result and hi-fg frames to video output
-    writeToVideo(&highlighted_fg_video,false);
-    writeToVideo(&tracking_result_video,true);
+    // add result and hi-fg frames to video output
+//    writeToVideo(&highlighted_fg_video,false);
+//    writeToVideo(&tracking_result_video,true);
 
     // display result
     try                                              // INFO //
@@ -488,7 +379,7 @@ void processVideo(char* videoFilename)
       exit(EXIT_FAILURE);                            //
     }                                                //
     // quit upon user input                          // INFO //
-    keyboard = waitKey((int)1000.0/capture.get(CV_CAP_PROP_FPS) ); // delay in millisec
+    keyboard = waitKey(10); //(int)1000.0/capture.get(CV_CAP_PROP_FPS) ); // delay in millisec
   }
   // delete capture object
   capture.release();
---
+++ pyscripts/undistort_points.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+* This script was borrowed from Dave C Williams *
+distortion.py - Dedistort and perspective transform points and images
+according to which camera or distortion model is passed.
+"""
+
+import cv, cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+
+#Default distortion and perspective matrices
+cam_distortion = np.array([-0.20724,0.02757,0.00007,-0.00041], dtype='float32')
+cam_matrix = np.array([ [232.90480,0,        321.92218],
+                        [0,        234.09177,225.46547],
+                        [0,        0,          1 ] ], dtype='float32')
+new_cam_matrix = np.array([np.zeros((3,3),dtype='float32')])
+cam_perspective = np.identity(3, dtype='float32') # Was very close to identity so didn't bother with correction
+
+
+
+
+def undistort_points(points2,cam_matrix,distortion_profile):
+  global new_cam_matrix
+  """Apply a distortion profile to a set of points"""
+  # passing points2 generates error: "OpenCV Error: Assertion failed (CV_IS_MAT(_src) && CV_IS_MAT(_dst)..."
+  points3=np.array([points2])#np.array([[n] for n in points2])
+  undistPoints=cv2.undistortPoints(points3, cam_matrix,distortion_profile,None,cam_matrix)
+  return undistPoints[0]
+
+def loadFile(file_name):
+  return np.loadtxt(file_name,delimiter = ',')
+
+ax = plt.figure().add_subplot(111)
+def plot_data(hdata,mdata):
+  ax.scatter(hdata[:,0],hdata[:,1],s=5,c='c',marker="x",label="undist_human")
+  ax.scatter(mdata[:,1],mdata[:,2],s=5,c='r',marker="x",label="machine")
+  plt.xlabel("x in pels")
+  plt.ylabel("y in pels")
+  plt.legend(loc='upper right')
+  plt.title("human_tracked vs machine_tracked")
+  plt.savefig("human_vs_comp.png") #**call before show**#
+  plt.show()
+  return
+
+
+def main():
+  global new_cam_matrix
+  # initialize new camera matrix
+  new_cam_matrix=cv2.getOptimalNewCameraMatrix(cam_matrix,cam_distortion,(640,480),1)
+  print("camMatrix: \n"+str(cam_matrix)) #DEBUG#
+  print("distCoeffs:\n"+ str(cam_distortion)) #DEBUG#
+  print("newCamMatrix: \n"+str(new_cam_matrix)) #DEBUG#
+
+  if sys.argv < 3:
+    raise ValueError("Incorrect number of arguments")
+  else:
+    dist_h_data = loadFile(sys.argv[1]) # col1=x, col2=y
+    m_data = loadFile(sys.argv[2]) # col1=frame#, col2=x, col3=y
+
+
+  undist_h_data=undistort_points(dist_h_data,cam_matrix,cam_distortion)
+
+  print("\ndistData:\n"+str(dist_h_data)) #DEBUG#
+  print("undistData:\n"+str(undist_h_data)) #DEBUG#
+  #ax.scatter(dist_h_data[:,0],dist_h_data[:,1],s=5,c='y',marker="x",label="dist_human")
+  plot_data(undist_h_data,m_data)
+
+  return
+
+
+
+main()
