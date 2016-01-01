7df7b39f9cc572c40b8a9f82313a18054bdf4290
Bilkit <githinjibcw@hotmail.com>
2016-01-01 15:15:18 -0800

    remove matlab

--- matlab_code/test_lensdistort.m
+++
@@ -1,8 +0,0 @@
-k_distortion = -0.45; % use -1 < k < 0 to undistort fish eye effect 
-x = imread('frame','png');
-y = lensdistort(x,k_distortion,'bordertype','crop','ftype',3); 
-% y = lensdistort(x,k_distortion,'ftype',2); 
-
-figure, subplot(121), imshow(x),title('original');
-subplot(122), imshow(y),...
-    title(sprintf('undistorted with k=%.2f',k_distortion));
